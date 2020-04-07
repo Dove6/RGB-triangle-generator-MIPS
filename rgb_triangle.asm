bb392f5b8a96bbebd429b65f6fb47e674d46ea03
Dawid Sygocki <d.sygocki@stud.elka.pw.edu.pl>
2020-04-07 21:11:44 +0200

    Refactor code layout
    Create the main function and... other functions
    
    (implement only the one responsible for memory allocation)

--- rgb_triangle.asm
+++ rgb_triangle.asm
@@ -1,74 +1,120 @@
+.eqv BITMAP_WIDTH 256
+.eqv BITMAP_HEIGHT 256
+
 .data
-		.half -1 # padding
+	.half -1 # padding (for word-aligning bitmap header)
 	BITMAPFILEHEADER:
-	bfType:
-		.ascii "BM"
-	bfSize:
-		.word 14
-	bfReserved1:
-		.half 0
-	bfReserved2:
-		.half 0
-	bfOffBits:
-		.word 54
+	.ascii "BM" # (+0x0) bfType
+	.word 14    # (+0x2) bfSize
+	.half 0     # (+0x6) bfReserved1
+	.half 0     # (+0x8) bfReserved2
+	.word 54    # (+0xa) bfOffBits
 
 	BITMAPINFOHEADER:
-	biSize:
-		.word 40
-	biWidth:
-		.word 256
-	biHeight:
-		.word 256
-	biPlanes:
-		.half 1
-	biBitCount:
-		.half 24
-	biCompression:
-		.word 0
-	biSizeImage:
-		.word 196608
-	biXPelsPerMeter:
-		.word 0
-	biYPelsPerMeter:
-		.word 0
-	biClrUsed:
-		.word 0
-	biClrImportant:
-		.word 0
+	.word 40 # (+0x00) biSize
+	.word 0  # (+0x04) biWidth (to specify)
+	.word 0  # (+0x08) biHeight (to specify)
+	.half 1  # (+0x0c) biPlanes
+	.half 24 # (+0x0e) biBitCount
+	.word 0  # (+0x10) biCompression
+	.word 0  # (+0x14) biSizeImage (to specify)
+	.word 0  # (+0x18) biXPelsPerMeter (ignored)
+	.word 0  # (+0x1c) biYPelsPerMeter (ignored)
+	.word 0  # (+0x20) biClrUsed
+	.word 0  # (+0x24) biClrImportant
 
 	output_filename:
-		.asciiz "result.bmp"
+	.asciiz "result.bmp"
 	vertex1_position:
-		.half 128 # x
-		.half 10  # y
+	.word 128 # x
+	.word 10  # y
 	vertex1_color:
-		.byte 0   # b
-		.byte 0   # g
-		.byte 255 # r
+	.byte 0   # b
+	.byte 0   # g
+	.byte 255 # r
 	vertex2_position:
-		.half 10  # x
-		.half 240 # y
+	.word 10  # x
+	.word 240 # y
 	vertex2_color:
-		.byte 0   # b
-		.byte 255 # g
-		.byte 0   # r
+	.byte 0   # b
+	.byte 255 # g
+	.byte 0   # r
 	vertex3_position:
-		.half 245 # x
-		.half 230 # y
+	.word 245 # x
+	.word 230 # y
 	vertex3_color:
-		.byte 255 # b
-		.byte 0   # g
-		.byte 0   # r
+	.byte 255 # b
+	.byte 0   # g
+	.byte 0   # r
 	bkg_color:
-		.align 2
-		.byte 0   # b
-		.byte 255 # g
-		.byte 255 # r
+	.align 2
+	.byte 0   # b
+	.byte 255 # g
+	.byte 255 # r
 	image_data:
-		.align 2
-		.space 196608
+	.align 2
+	.space 196608
 
 .text
+main:
+	# [prepare bitmap header and data]
+	li $a0, BITMAP_WIDTH	
+	li $a1, BITMAP_HEIGHT
+	jal generate_bitmap
+
+	# [process bitmap]
+	# [save to file]
+	# [destroy bitmap]
+	# <nothing>
+
+	exit:
+	li $v0, 10
+	syscall
+
+
+	# parameters:
+	# $a0 - width
+	# $a1 - height
+	# returns:
+	# $v0 - pointer to BITMAPINFOHEADER
+	# $v1 - pointer to image data
+generate_bitmap:
+	move $t0, $a0
+	move $t1, $a1
+	lw $t2, BITMAPINFOHEADER
+	# allocate memory for bitmap header
+	li $v0, 9
+	move $a0, $t2
+	syscall
+	move $t3, $v0
+	# copy template data to allocated header
+	move $t4, $zero
+	header_copy_loop:
+	lb $t5, BITMAPINFOHEADER($t4)
+	addu $t6, $t3, $t4
+	sb $t5, ($t6)
+	addiu $t4, $t4, 1
+	blt $t4, $t2, header_copy_loop
+	# set variable header data
+	sw $t0, 4($t3) # width
+	sw $t1, 8($t3) # height
+	mulu $t4, $t0, $t1
+	sll $t5, $t4, 1    # }
+	addu $t5, $t5, $t4 # } multiply by 3
+	sw $t5, 20($t3) # pixel data size
+	# TODO: error on too large data
+	# allocate space for pixel data
+	li $v0, 9
+	move $a0, $t5
+	syscall
+	# place returned pointers in v-registers
+	move $v1, $v0
+	move $v0, $t3
+	jr $ra
+	
+
+
+
 		# rearrange vertices
 		
 		# place useful data in registers
@@ -77,87 +123,82 @@
 		# s2 - right side X position
 		# s3 - right side color
 		# s4 - background color
-		lw $s4, bkg_color
+#		lw $s4, bkg_color
 		# s5 - memory row base
 		
 		# initialize the loop
 		# t0 - row counter
-		lw $t0, biHeight
-		subiu $t0, $t0, 1
+#		lw $t0, biHeight
+#		subiu $t0, $t0, 1
 		# t1 - column counter
 		# t2 - ?
 		# t3 - fill color
-		move $t3, $zero
+#		move $t3, $zero
 		
-	row_loop:
-		bltz $t0, write_file
-		lw $t1, biWidth
-		subiu $t1, $t1, 1
+#	row_loop:
+#		bltz $t0, write_file
+#		lw $t1, biWidth
+#		subiu $t1, $t1, 1
 		# calculate left and right side
 		
 		# calculate row memory offset
-		lw $s5, biHeight
-		subiu $s5, $s5, 1
-		subu $s5, $s5, $t0
-		sll $s5, $s5, 8 # multiply by 256
-		move $t9, $s5
-		sll $s5, $s5, 1
-		addu $s5, $s5, $t9 # multiply by 3 (bytes per pixel)
+#		lw $s5, biHeight
+#		subiu $s5, $s5, 1
+#		subu $s5, $s5, $t0
+#		sll $s5, $s5, 8 # multiply by 256
+#		move $t9, $s5
+#		sll $s5, $s5, 1
+#		addu $s5, $s5, $t9 # multiply by 3 (bytes per pixel)
 		
-	column_loop:
-		bltz $t1, row_loop_end
-		move $t3, $s4
+#	column_loop:
+#		bltz $t1, row_loop_end
+#		move $t3, $s4
 		# calculate row + column memory offset
-		move $t9, $s5
-		sll $t8, $t1, 1
-		addu $t8, $t8, $t1 # multiply by 3 (bytes per pixel)
-		addu $t9, $t9, $t8
+#		move $t9, $s5
+#		sll $t8, $t1, 1
+#		addu $t8, $t8, $t1 # multiply by 3 (bytes per pixel)
+#		addu $t9, $t9, $t8
 		
-		sb $t3, image_data($t9)
-		srl $t3, $t3, 8
-		sb $t3, image_data+1($t9)
-		srl $t3, $t3, 8
-		sb $t3, image_data+2($t9)
-		subiu $t1, $t1, 1
-		j column_loop
+#		sb $t3, image_data($t9)
+#		srl $t3, $t3, 8
+#		sb $t3, image_data+1($t9)
+#		srl $t3, $t3, 8
+#		sb $t3, image_data+2($t9)
+#		subiu $t1, $t1, 1
+#		j column_loop
 	
-	row_loop_end:
-		subiu $t0, $t0, 1
-		j row_loop
+#	row_loop_end:
+#		subiu $t0, $t0, 1
+#		j row_loop
 
-	write_file:
+#	write_file:
 		# open output file
-		li $v0, 13
-		la $a0, output_filename
-		li $a1, 1 # write-only mode
-		syscall
-		bltz $v0, exit
-		move $t0, $v0
+#		li $v0, 13
+#		la $a0, output_filename
+#		li $a1, 1 # write-only mode
+#		syscall
+#		bltz $v0, exit
+#		move $t0, $v0
 
 		# write BMP headers to file
-		li $v0, 15
-		move $a0, $t0
-		la $a1, BITMAPFILEHEADER
-		lw $a2, bfSize
-		lw $t9, biSize
-		addu $a2, $a2, $t9
-		syscall
+#		li $v0, 15
+#		move $a0, $t0
+#		la $a1, BITMAPFILEHEADER
+#		lw $a2, bfSize
+#		lw $t9, biSize
+#		addu $a2, $a2, $t9
+#		syscall
 		# TODO: error check
 
 		# write image contents to file
-		li $v0, 15
-		move $a0, $t0
-		la $a1, image_data
-		lw $a2, biSizeImage
-		syscall
+#		li $v0, 15
+#		move $a0, $t0
+#		la $a1, image_data
+#		lw $a2, biSizeImage
+#		syscall
 		# TODO: error check
 
 		# close the file
-		li $v0, 16
-		move $a0, $t0
-		syscall
-
-		# exit
-	exit:
-		li $v0, 10
-		syscall
+#		li $v0, 16
+#		move $a0, $t0
+#		syscall
